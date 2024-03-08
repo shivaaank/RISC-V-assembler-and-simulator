08ead2da9ab530ce4a52ac47598fab8a8b0bc3bb
shivank <shivank.rajput2005@gmail.com>
2024-03-08 23:59:40 +0530

    tokenize

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
+from typing import Any
 from assembler import *
+
+def Parse(x):
+    #check if valid:
+    if (x[0] == "#" or x[0] == "\n" or x[0] == "" or x[0] == "."):
+        return ''
+    #handle inline comments, replace , with space
+    if "#" in x:
+        pos = x.index("#")
+        if pos != 0 and pos != len(x)-1:
+            x = x[0:pos].replace(',', ' ')
+    else:
+        x = x.replace(',', ' ')
+    
+    tokens = x.split(' ')
+    return tokens
+
+
 
 #read and store input from file
 with open("input.txt") as f:
@@ -7,4 +25,5 @@ with open("input.txt") as f:
 with open("output.txt", "w") as f:
     for line in inp_lines:
         out_line = Parse(line)
-        f.writeline(out_line)
+        print(out_line)
+        # f.writeline(out_line)
