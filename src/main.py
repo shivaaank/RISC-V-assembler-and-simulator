ff1f3741bb4a9172e8b0c41ea3b1ef4709db16b9
RohanS. <rohan23443@iiitd.ac.in>
2024-03-10 20:39:39 +0530

    a

--- src/main.py
+++ src/main.py
@@ -59,7 +59,7 @@ def Parse(x : str, pc : int)-> str:
 #read and store input from file
 with open("src\input.txt") as f:
     inp_lines = [i.strip('\n') for i in f]
-inp_lines = [i for i in inp_lines if i != '']
+inp_lines = [i for i in inp_lines if i != ''] #remove empty lines
 length = len(inp_lines)
 print(inp_lines)
 count = 0
