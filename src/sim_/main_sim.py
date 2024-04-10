e533c2abd12b68cdda170e4df096b361e181f698
shivank <shivank.rajput2005@gmail.com>
2024-04-11 00:44:32 +0530

    r type

--- src/sim_/main_sim.py
+++ src/sim_/main_sim.py
@@ -2,11 +2,11 @@ from simulator import simulator
 
 pc = 0
 
-with open('src\sim_input.txt','r') as f:
+with open('sim_input.txt','r') as f:
     instruc = f.readlines()
     instruc = [i.strip('\n') for i in instruc]
 
-with open('src\sim_output.txt','w') as f:
+with open('sim_output.txt','w') as f:
     f.write('')
 
 sim = simulator(instruc,pc)
