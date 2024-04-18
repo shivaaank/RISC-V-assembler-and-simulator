7e87a21ed6b28cd045a1f7b7b93783b94d722e99
shivank <shivank.rajput2005@gmail.com>
2024-04-18 06:14:10 +0530

    pls work

--- src/sim_/simulatorclass.py
+++ src/sim_/simulatorclass.py
@@ -1,21 +1,3 @@
-from sim_.simulatorclass import simulator
-import sys
-
-inpf = sys.argv[1]
-outpf = sys.argv[2]
-
-pc = 0
-
-with open(inpf,'r') as f:
-    instruc = f.readlines()
-    instruc = [i.strip('\n') for i in instruc]
-
-sim = simulator(instruc,pc)
-sim.execute(outpf)
-
-with open(outpf,'w') as f:
-    f.write("")
-
 from instruc import *
 #---------------DONT DELETE THIS FOR NOWwWWWWWWWWWWWWWWWWWWW--------------
 # def read(instruc):
